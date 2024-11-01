5dbc5c341d49f09652a9b7b7ea285021f33516ae
Tianyi Zheng <zhengty0509@gmail.com>
2024-11-01 23:55:46 +0900

    Create Avalonia .NET project

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace INOPASE_SNM_desktop_software;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
